e1f842d2cf4429db52f659dd2ddd8ed6499ddb09
barbapaue <barbapaule@gmail.com>
2022-01-04 15:36:34 +0100

    fix documentation and regex pattern in validate regular function

--- src/main/kotlin/TaxCodeUtility.kt
+++ src/main/kotlin/TaxCodeUtility.kt
@@ -2,28 +2,42 @@ import java.util.*
 import kotlin.jvm.Throws
 
 /**
- * Validazione di un codice fiscale, utilizando come codice base
+ * Created by Paolo Pasianot on 04/01/22.
+ */
+
+/**
+ * Library heavily inspired by:
  * https://www.icosaedro.it/cf-pi/vedi-codice.cgi?f=cf-java.txt
  */
 
 /**
  * Verifies the basic syntax, length and control code of the given CF.
+ * @throws CFInvalidCharacters if there are any invalid characters
+ * @throws CFInvalidChecksum if the composition of the words is not correct
+ * @throws CFIsEmpty if the string is empty
+ * @throws CFInvalidLength if the string is outside the length parameters
+ * @return validate regular cf
  */
-@Throws(CFInvalidCharacters::class, CFInvalidChecksum::class, CFIsEmpty::class)
-fun String.validateCF(): String = with(normalizeCF()) {
-    when {
-        isNullOrBlank() -> throw CFIsEmpty()
-        length == 16 -> validateRegularCF()
-        length == 11 -> validateTemporaryCF()
-        else -> throw CFInvalidLength()
+@Throws(
+    CFInvalidCharacters::class,
+    CFInvalidChecksum::class,
+    CFIsEmpty::class,
+    CFInvalidLength::class
+)
+fun String.validateCF(): String =
+    with(normalizeCF()) {
+        when {
+            isNullOrBlank() -> throw CFIsEmpty()
+            length == 16 -> validateRegularCF()
+            length == 11 -> validateTemporaryCF()
+            else -> throw CFInvalidLength()
+        }
     }
-}
 
 
 /**
  * Normalizes a CF by removing white spaces and converting to upper-case.
  * Useful to clean-up user's input and to save the result in the DB.
- * @return Normalized CF.
  */
 private fun String.normalizeCF(): String =
     replace("[ \t\r\n]".toRegex(), "")
@@ -31,12 +45,18 @@ private fun String.normalizeCF(): String =
 
 /**
  * Validates a regular CF.
+ * @throws CFInvalidCharacters if there are any invalid characters
+ * @throws CFInvalidChecksum if the composition of the words is not correct
  * @return validate regular cf
- * @throws CFInvalidChecksum if string contain wrong chars
  */
-@Throws(CFInvalidCharacters::class, CFInvalidChecksum::class)
-fun String.validateRegularCF(): String {
-    if (!this.matches("^[0-9A-Z]{16}$".toRegex())) throw CFInvalidCharacters()
+@Throws(
+    CFInvalidCharacters::class,
+    CFInvalidChecksum::class
+)
+private fun String.validateRegularCF(): String {
+    val pattern = "^[A-Z]{6}[0-9LMNPQRSTUV]{2}[ABCDEHLMPRST][0-9LMNPQRSTUV]{2}[A-Z][0-9LMNPQRSTUV]{3}[A-Z]$".toRegex()
+    //val oldPattern = "^[0-9A-Z]{16}$"
+    if (!this.matches(pattern)) throw CFInvalidCharacters()
     var s = 0
     val evenMap = "BAFHJNPRTVCESULDGIMOQKWZYX"
     for (index in 0..14) {
@@ -50,9 +70,14 @@ fun String.validateRegularCF(): String {
 
 /**
  * Validates a temporary CF.
- * @return Null if valid, or string describing why this CF must be rejected.
+ * @throws CFInvalidCharacters if there are any invalid characters
+ * @throws CFInvalidChecksum if the composition of the words is not correct
+ * @return validate temporary cf
  */
-@Throws(CFInvalidCharacters::class, CFInvalidChecksum::class)
+@Throws(
+    CFInvalidCharacters::class,
+    CFInvalidChecksum::class
+)
 private fun String.validateTemporaryCF(): String {
     if (!matches("^[0-9]{11}$".toRegex())) throw CFInvalidCharacters()
     var s = 0
