9d73cbbea039b934a685be4096d2b49c7153c18e
barbapaue <barbapaule@gmail.com>
2022-01-05 11:09:41 +0100

    jitpack problem solving

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.6.10"
     `java-library`
+    `maven-publish`
 }
 
 repositories {
@@ -20,6 +21,49 @@ testing {
         val test by getting(JvmTestSuite::class) {
             useKotlinTest()
             useJUnitJupiter()
+        }
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "it.paolopasianot.italiantaxcodevalidator"
+            artifactId = "library"
+            version = "1.0.2"
+
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("Italian Tax Code Validator")
+                description.set("This small library was developed to validate Italian tax codes.")
+                url.set("https://github.com/Barbapaue/ItalianTaxCodeValidator")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("paolopasianot")
+                        name.set("Paolo Pasianot")
+                        email.set("paolo.pasianot@icloud.com")
+                    }
+                }
+            }
         }
     }
 }
